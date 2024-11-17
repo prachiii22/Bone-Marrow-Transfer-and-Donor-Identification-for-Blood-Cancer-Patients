e2b0117eddce9768d698c3fdb92b20b68e543e45
prachiii22 <prachiii.p22@gmail.com>
2024-11-17 20:00:12 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add_patient/', views.add_patient, name='add_patient'),
-    path('add_donor/', views.add_donor, name='add_donor'),
-    path('predict_compatibility/', views.predict_compatibility, name='predict_compatibility'),
-]
